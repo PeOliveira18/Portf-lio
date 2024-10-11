78db021c9b3d267e15df29483649e0d5bf366d7a
Pedro Oliveira <oliveirasilvapedro2@gmail.com>
2024-10-11 11:51:40 -0300

    Finalizando

--- src/Components/Hero/hero.jsx
+++ src/Components/Hero/hero.jsx
@@ -8,7 +8,7 @@ function Hero() {
         <section className="flex flex-col m-auto pt-36 pb-[144px] text-center max-w-[736px]">
             <div className="gap-3 flex flex-col">
                 <h1 className='mt-4 text-black dark:text-white-100 md:text-6xl text-3xl'>Olá, meu nome é Pedro</h1>
-                <h1 className="text-texto-100 md:text-5xl text-3xl">Engenheiro de Software</h1>
+                <h1 className="text-texto-100 md:text-5xl text-2xl">Engenheiro de Software</h1>
                 <img src={minhaFoto} alt="Foto de Pedro Oliveira" className='md:w-36 w-28 m-auto mt-5'/>
             </div>
             <div className="mt-10">
